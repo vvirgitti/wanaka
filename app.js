3d8b866b5fd1b2e7b9634bdbb439c8f4481472c4
Vanessa Virgitti <virgitti@gmail.com>
2016-05-24 14:06:14 +0100

    Added handlebars - WIP

--- app.js
+++ app.js
@@ -4,23 +4,37 @@ require('babel-register');
 
 import express from 'express';
 import bodyParser from 'body-parser';
+import handlebars from 'handlebars';
+import exphbs from 'express-handlebars';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.set('view engine', 'html');
-app.set('views', __dirname);
-app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/views');
+app.set('styling', __dirname + '/styling');
+
+const hbs = exphbs.create({
+  defaultLayout: 'layout',
+  extname: '.hbs',
+  layoutsDir: __dirname + '/views',
+  partialsDir: __dirname + '/views/partials'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+
+handlebars.registerPartial('header', '{{header}}')
+
 app.use(express.static(__dirname));
 
 app.get('/', function(req, res){
-  res.sendFile('index.html', {root: __dirname});
+  res.render('layout.hbs', {root: __dirname + '/views'});
 });
 
 app.get('/day', function(req, res){
-  res.sendFile('day.html', {root: __dirname/views});
+  res.render('/views/day.hbs', {root: __dirname});
 });
 
 module.exports = app;
