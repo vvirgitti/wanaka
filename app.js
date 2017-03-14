62c341af1909db5b72af5b9ce7e9c322189b334c
Vanessa Virgitti <virgitti@gmail.com>
2017-03-14 16:39:20 +0000

    New template

--- app.js
+++ app.js
@@ -6,43 +6,27 @@ const handlebars = require('handlebars');
 const exphbs = require('express-handlebars');
 const path = require('path')
 
-const enRouter = require('./routes/en');
-
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.set('views', path.join(__dirname + '/views'));
-app.set('styling', path.join(__dirname + '/styling'));
+app.set('img', path.join(__dirname + '/img'));
+app.set('css', path.join(__dirname + '/css'));
 
 const hbs = exphbs.create({
   defaultLayout: 'layout',
   extname: '.hbs',
-  layoutsDir: __dirname + '/views',
-  partialsDir: __dirname + '/views/partials'
+  layoutsDir: __dirname + '/img'
 });
 
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
-handlebars.registerPartial('header', '{{header}}');
-handlebars.registerPartial('footer', '{{footer}}');
-
 app.use(express.static(__dirname));
 
-app.use('/en', enRouter);
-
 app.get('/', function(req, res){
-  res.render('index-en');
-});
-
-app.get('/en', function(req, res){
-  res.render('index-en');
-});
-
-app.get('/fr', function(req, res){
-  res.render('index-fr');
+  res.render('index');
 });
 
 module.exports = app;
